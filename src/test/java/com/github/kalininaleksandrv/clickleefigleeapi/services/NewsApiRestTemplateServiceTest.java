c2e9a2f3fd73e2254d49ce1c42811a39c39ceb77
kalininaleksandrv <kalinin.aleksandr.v@gmail.com>
2020-03-05 11:06:51 +0300

    change savetodb method in CustomMessageService in more functional way

---
+++ src/test/java/com/github/kalininaleksandrv/clickleefigleeapi/services/NewsApiRestTemplateServiceTest.java
@@ -0,0 +1,16 @@
+package com.github.kalininaleksandrv.clickleefigleeapi.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NewsApiRestTemplateServiceTest {
+
+    @Test
+    void init() {
+    }
+
+    @Test
+    void getLatestNewsFromApi() {
+    }
+}
